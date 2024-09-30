f9c7de2b682153421ed7f60e037e3b60631c1dba
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-09-30 14:45:44 +0300

    modified 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,17 +1,8 @@
 export default function createReportObject(employeesList) {
-  let count = 0;
-  let obj = {};
-  //   console.log(employeesList);
-  //   for (const key of Object.keys(employeesList)) {
-  //     console.log(key);
-  //     console.log(...employeesList[key]);
-  //   }
   return {
     allEmployees: employeesList,
     getNumberOfDepartments(employeesList) {
-      for (const key of Object.keys(employeesList)) {
-        count++;
-      }
+      const count = Object.keys(employeesList).length;
       return count;
     },
   };
