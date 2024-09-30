6f7378fc4752fa82afc8251d93b34ad970b156a8
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-09-30 13:01:45 +0300

    added test in 0-constants-test.js

--- 0x00-ES6_basic/tests/0-constants.test.js
+++ 0x00-ES6_basic/tests/0-constants.test.js
@@ -1,4 +1,12 @@
-import { taskFirst } from "../0-constants";
-test("return : {I prefer const when I can.}", () => {
+import { taskFirst, getLast, taskNext } from "../0-constants";
+test("taskFirst --> returns -> {I prefer const when I can.}", () => {
   expect(taskFirst()).toBe("I prefer const when I can.");
+});
+
+test("getLast --> returns -> is okay", () => {
+  expect(getLast()).toBe(" is okay");
+});
+
+test("taskNext --> returns -> But sometimes let is okay", () => {
+  expect(taskNext()).toBe("But sometimes let is okay");
 });
