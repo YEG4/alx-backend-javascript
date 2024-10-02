e6a6ffbe5c05e2b26c9b3311272522b2e7fd5fb6
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-02 15:27:32 +0300

    Modified 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,5 +1,8 @@
 export default function divideFunction(numerator, denominator) {
   try {
+    if (denominator === 0) {
+      throw Error('cannot divide by 0');
+    }
     const result = numerator / denominator;
     return result;
   } catch (e) {
