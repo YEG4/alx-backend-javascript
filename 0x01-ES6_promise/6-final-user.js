6cb91b2455f155fd7d0499501db7c255685d09d4
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-02 15:07:24 +0300

    Modified 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => {
+  ]).then((results) =>
     results.map((result) => ({
       status: result.status,
       value:
         result.status === 'fulfilled' ? result.value : String(result.reason),
-    }));
-  });
+    }))
+  );
 }
