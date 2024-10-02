1024d1c3722c871b6f5a1d8f234fcf849727924f
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-02 14:24:23 +0300

    Modified 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,11 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   uploadPhoto()
     .then((res) => {
-      createUser().then((response) => {
-        console.log(`${res.body} ${response.firstName} ${response.lastName}`);
-      });
+      createUser()
+        .then((response) => {
+          console.log(`${res.body} ${response.firstName} ${response.lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
     })
     .catch(() => console.log('Signup system offline'));
 }
