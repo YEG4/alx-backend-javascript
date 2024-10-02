63cc17dc94f0d058e780f1c2b40749bb59867a67
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-02 13:33:11 +0300

    Modified 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => new Promise({ status: 200, body: 'success' }))
-    .catch(() => new Error({}))
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
