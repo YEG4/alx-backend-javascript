c7e1ba0157f950ef2f49bc34b28de8f457eceaf7
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-03 15:50:08 +0300

    Added 100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    /* eslint-disable-next-line no-underscore-dangle */
+    this._range = range;
+  }
+
+  cloneCar() {
+    /* eslint-disable-next-line no-underscore-dangle */
+    return new super.constructor(this._brand, this._motor, this._colores);
+  }
+}
