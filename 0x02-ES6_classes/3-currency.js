286148a9e51a9ec5f54266ac7d19d4c8506193a1
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-10-03 13:57:28 +0300

    Added 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,8 +1,31 @@
 export default class Currency {
   constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
     /* eslint-disable-next-line no-underscore-dangle */
-    this._code = code;
+    return this._code;
+  }
+
+  set code(value) {
     /* eslint-disable-next-line no-underscore-dangle */
-    this._name = name;
+    this._code = value;
+  }
+
+  get name() {
+    /* eslint-disable-next-line no-underscore-dangle */
+    return this._name;
+  }
+
+  set name(value) {
+    /* eslint-disable-next-line no-underscore-dangle */
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    /* eslint-disable-next-line no-underscore-dangle */
+    return `${this._name} (${this._code})`;
   }
 }
