c4f523c8e2e3bcb6ac802ae4a0e0c8aaf52d2678
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-11-26 16:32:31 +0200

    modified 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,11 @@
+/**
+ * Displays a message to STDOUT
+ * @param String message the message to display
+ * @author Y3GA
+ */
+
 function displayMessage(message) {
-  process.stdout.write(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;
